39a07f77923dd0d354ceed54b79a494429945e07
philipp-vogel <philipp242@live.de>
2022-11-02 14:42:45 +0100

    Set validation pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,17 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //Set globally validation type for useage in the dto's
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Whitelist allows only key/values in request which where predefined in dto's. Protection against incorrect entries and prevents vulnerabilities.
+    }),
+  );
 
   //Setup Swagger
   const config = new DocumentBuilder()
