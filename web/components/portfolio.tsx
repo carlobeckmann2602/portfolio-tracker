d236f4f43b70cc7e20020423d026dd28a3e645d4
Marco Mojica <marco@mojica.de>
2022-11-04 15:26:22 +0100

    Add Components for Portfolio and Search

---
+++ web/components/portfolio.tsx
@@ -0,0 +1,69 @@
+import { Chart } from "react-google-charts"
+
+const treeMapData = [
+    [
+        "Name",
+        "Parent",
+        "Ammount",
+        ""
+    ],
+    ["Portfolio", null, 0, 0],
+    ["StockA", "Portfolio", 1, 0],
+    ["StockB", "Portfolio", 2, 0],
+    ["StockC", "Portfolio", 3, 0],
+    ["StockD", "Portfolio", 4, 0],
+];
+
+const treeMapOptions = {
+    minColor: "#f00",
+    midColor: "#ddd",
+    maxColor: "#0d0",
+    headerHeight: 0,
+    fontColor: "black",
+
+    eventsConfig: {
+        highlight: [],
+        unhighlight: [],
+        rollup: [],
+        drilldown: [],
+        select: []
+    }
+};
+
+const pieChartData = [
+    ["Stock", "Ammount"],
+    ["StockA", 1],
+    ["StockB", 2],
+    ["StockC", 3],
+    ["StockD", 4],
+];
+
+const pieChartOptions = {
+    title: "My Stock Portfolio",
+};
+
+const Portfolio = () => {
+    return (
+        <>
+            <h1>Portfolio</h1>
+            <div className="flex-col items-center justify-between">
+                <Chart
+                    chartType="TreeMap"
+                    width="100%"
+                    height="400px"
+                    data={treeMapData}
+                    options={treeMapOptions}
+                />
+                <Chart
+                    chartType="PieChart"
+                    width={"100%"}
+                    height={"400px"}
+                    data={pieChartData}
+                    options={pieChartOptions}
+                />
+            </div>
+        </>
+    )
+}
+
+export default Portfolio
