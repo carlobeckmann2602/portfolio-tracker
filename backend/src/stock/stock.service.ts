86ab1a6a7147d3c6e29791db5595e4daf330caf1
philipp-vogel <philipp242@live.de>
2022-11-05 15:46:54 +0100

    adding stock to user
    Stock can be added to user

--- backend/src/stock/stock.service.ts
+++ backend/src/stock/stock.service.ts
@@ -1,11 +1,36 @@
-import { Injectable } from '@nestjs/common';
-import { CreateStockDto } from './dto/create-stock.dto';
-import { UpdateStockDto } from './dto/update-stock.dto';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class StockService {
-  create(createStockDto: CreateStockDto) {
-    return 'This action adds a new stock';
+  constructor(private prisma: PrismaService) {}
+  async addStockToUser(id: number, sid: number) {
+    try {
+      //set relation between stock and user and updates stock in db
+      const stock = await this.prisma.stock.update({
+        where: {
+          id: sid,
+        },
+        data: {
+          userId: id,
+        },
+      });
+
+      //return??? redirect auf portfolio Seite?
+      return `This action adds a stock with ID:${sid} to user with ID: ${id}`;
+    } catch (error) {
+      //caching prismas notFound error P2025
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2025') {
+          throw new NotFoundException('Stock not found');
+        }
+      }
+      throw error;
+    }
+
+    //hier muss noch Logic hin um zu ermitteln wie viele stocks der user von einer sorte besitzt
+    //momentan bekommt er die stocks einfach zugewiesen
   }
 
   findAll() {
@@ -16,11 +41,7 @@ export class StockService {
     return `This action returns a #${id} stock`;
   }
 
-  update(id: number, updateStockDto: UpdateStockDto) {
-    return `This action updates a #${id} stock`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} stock`;
+  removeStockFromUser(id: number, sid: number) {
+    return `This action removes a stock with ID:${sid} from user with ID: ${id}`;
   }
 }
