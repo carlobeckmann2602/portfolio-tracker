614411b503936cc2dd09e75b27a8a45a459d04c6
philipp-vogel <philipp242@live.de>
2022-11-05 14:26:35 +0100

    Tryout Post auf DB
    Adding some logic to user.service. Service can now create a user object in database.

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto/auth.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  login(authDto: AuthDto) {
+    console.log(authDto);
+
+    return 'User logged in';
+  }
+  logout() {
+    return 'User logged out';
+  }
+}
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,19 +1,58 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import * as argon from 'argon2';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  //enables prisma client db operations like save in the db
+  constructor(private prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    //generate password haswith argon
+    const hash = await argon.hash(createUserDto.password);
+
+    //check if user is already in db (email should be unique)
+    try {
+      //save user in db
+      const user = await this.prisma.user.create({
+        data: {
+          email: createUserDto.email,
+          hash,
+        },
+        select: {
+          //"Select" allows us to send wanted data back to the client.
+          id: true,
+          email: true,
+        },
+      });
+      //return saved user (only selected fields)
+      return user;
+    } catch (error) {
+      //caching prismas unique duplicate error P2002
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+      throw error;
+    }
   }
 
-  findAll() {
-    return `This action returns all users`;
-  }
+  async findOne(id: number) {
+    //search in db with unique id
+    const user = await this.prisma.user.findFirst({
+      where: {
+        id: id,
+      },
+    });
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+    if (user === null) {
+      throw new NotFoundException();
+    }
+    return user;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
