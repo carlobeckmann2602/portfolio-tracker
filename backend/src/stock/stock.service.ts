1f85f20ee82d4da28cfbbca6d2f9de961e86d158
philipp-vogel <philipp242@live.de>
2022-11-08 19:01:56 +0100

    Add stock to user corrected
    Now the user can add a stock to portfolio with given amount. Dto for amount implemented

--- backend/src/stock/stock.service.ts
+++ backend/src/stock/stock.service.ts
@@ -1,26 +1,38 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 import { PrismaService } from 'src/prisma/prisma.service';
-
+import { Stock } from '@prisma/client';
+import { AddStockToUserDto } from 'src/user/dto/add-stock-to-user';
 @Injectable()
 export class StockService {
   constructor(private prisma: PrismaService) {}
-  async addStockToUser(id: number, sid: number) {
+  async addStockToUser(id: number, sid: number, addStockToUserDto: AddStockToUserDto) {
     try {
       //set relation between stock and user and updates stock in db
-      const stock = await this.prisma.stock.update({
+      const stock = await this.prisma.stocksOnUsers.upsert({
         where: {
-          id: sid,
+          userId_stockId: {
+            userId: id,
+            stockId: sid,
+          },
         },
-        data: {
+        update: {
+          amount: {
+            increment: +addStockToUserDto.amount, // amtomic number operation: Adds given amount to current amount
+          },
+        },
+        create: {
           userId: id,
+          stockId: sid,
+          amount: +addStockToUserDto.amount,
         },
       });
-
-      //return??? redirect auf portfolio Seite?
+      //return? All stocks from user for portfolio page?
       return `This action adds a stock with ID:${sid} to user with ID: ${id}`;
     } catch (error) {
-      //caching prismas notFound error P2025
+      console.log(error.code);
+
+      //caching prismas notFound error P2001
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === 'P2025') {
           throw new NotFoundException('Stock not found');
@@ -37,11 +49,20 @@ export class StockService {
     return `This action returns all stocks`;
   }
 
-  findOne(id: number) {
+  async findOne(id: number) {
+    const stock = await this.prisma.stock.findUnique({
+      where: {
+        id: id,
+      },
+    });
+
+    return stock;
     return `This action returns a #${id} stock`;
   }
 
   removeStockFromUser(id: number, sid: number) {
+    try {
+    } catch (error) {}
     return `This action removes a stock with ID:${sid} from user with ID: ${id}`;
   }
 }
---
+++ backend/src/user/dto/add-stock-to-user.ts
@@ -0,0 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
+
+//Bug in Swagger -> not in Swagger UI!
+export class AddStockToUserDto {
+  //If user buys a stock a stock, he has to send infos about the amount and the stockId
+  @ApiProperty()
+  @IsNotEmpty()
+  amount: number;
+}
