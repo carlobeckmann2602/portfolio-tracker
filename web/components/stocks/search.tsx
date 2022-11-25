b9f12f761b53f4f767fd3c03f8dcf69565593647
Ben Kräling <ben.kraeling@study.hs-duesseldorf.de>
2022-11-25 08:56:00 +0100

    Implement temp function to filter stocks and display results

--- web/components/stocks/search.tsx
+++ web/components/stocks/search.tsx
@@ -1,29 +1,59 @@
 import React, { useEffect, useState } from "react";
+import { StockData, useStockHoldings } from "../../lib/backend";
 import { Input } from "../form/input";
 
 export type SearchProps = {};
 
 export const Search = ({}: SearchProps) => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [filteredStocks, setFilteredStocks] = useState<StockData[]>([]);
   const searchDelay = 500;
+  let filteredStocksList = filteredStocks.map((stock, index) => {
+    return <div key={index}>{stock.name}</div>;
+  });
 
   const handleSearchChange = (evt: Event) => {
     setSearchTerm((evt.target as HTMLTextAreaElement).value);
   };
 
-  useEffect(() => {
-    const delay = setTimeout(() => {
-      console.log(searchTerm);
-    }, searchDelay);
+  /* will be replaced when filter is implemented in backend */
+  const tempFilterStocksByName = (stocks: StockData[], searchTerm: string) => {
+    const filteredStocks: StockData[] = [];
+    stocks.forEach((stock) => {
+      if (
+        stock.name
+          .toLocaleLowerCase()
+          .startsWith(searchTerm.toLocaleLowerCase())
+      ) {
+        filteredStocks.push(stock);
+      }
+    });
+    return filteredStocks;
+  };
 
+  useEffect(() => {
+    const delay = setTimeout(async () => {
+      if (searchTerm === "") {
+        setFilteredStocks([]);
+        return;
+      }
+      const res = await fetch(
+        `https://api.mobilesys.de/stocks?name=${searchTerm}"`
+      );
+      const data = await res.json();
+      setFilteredStocks(tempFilterStocksByName(data, searchTerm));
+    }, searchDelay);
     return () => clearTimeout(delay);
   }, [searchTerm]);
 
   return (
-    <Input
-      handleChange={(evt: Event) => handleSearchChange(evt)}
-      name="searchTerm"
-      placeholder="Search for a stock"
-    />
+    <>
+      <Input
+        handleChange={(evt: Event) => handleSearchChange(evt)}
+        name="searchTerm"
+        placeholder="Search for a stock"
+      />
+      {filteredStocksList}
+    </>
   );
 };
