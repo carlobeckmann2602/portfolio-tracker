87c0a14a9a53ddefb5ab3d6dc39115803e7e4b1e
Paul Kretschel <paul.kretschel@study.hs-duesseldorf.de>
2022-12-11 18:00:35 +0100

    Fix adjacent donut chart items having same color

--- web/components/portfolio/donut-chart.tsx
+++ web/components/portfolio/donut-chart.tsx
@@ -17,21 +17,32 @@ const colors = ["#4666A2", "#547ECD", "#90AFE5", "#ACBEDE", "#C5D5EE"];
 const selectedSegmentOffset = 5;
 
 export const DonutChart = ({ items, selected, onClick }: DonutChartProps) => {
-  const segments = React.useMemo<DonutChartItem[]>(
-    () =>
-      items.length
-        ? items.map((item, i) => ({
-            value: item.value,
-            color: colors[i % colors.length],
-          }))
-        : [
-            {
-              value: 1,
-              color: "#efefef",
-            },
-          ],
-    [items]
-  );
+  const segments = React.useMemo<DonutChartItem[]>(() => {
+    if (!items.length) return [{ value: 1, color: "#efefef" }];
+
+    let getColorId = (id: number) => id % colors.length;
+
+    if (items.length > colors.length) {
+      const excessItemCount = items.length % colors.length;
+      if (excessItemCount) {
+        const excessIdOffset = Math.floor(excessItemCount / 2);
+        const excessStartId =
+          colors.length * Math.floor(items.length / colors.length);
+        const midColorId = Math.floor(colors.length / 2);
+
+        const getColorIdBase = getColorId;
+        getColorId = (id: number) =>
+          id < excessStartId
+            ? getColorIdBase(id)
+            : midColorId - excessIdOffset - excessStartId + id;
+      }
+    }
+
+    return items.map((item, i) => ({
+      value: item.value,
+      color: colors[getColorId(i)],
+    }));
+  }, [items]);
 
   const isEmpty = !items.length;
 
