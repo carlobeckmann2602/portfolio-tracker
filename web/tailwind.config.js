93d6d36b9db8c8ad3e66f952d35fdddc9a3e6c0d
kleinErdbeerchen <57522470+kleinErdbeerchen@users.noreply.github.com>
2022-12-11 17:09:06 +0100

    added general style configurations

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -7,13 +7,28 @@ module.exports = {
     extend: {
       colors: {
         white: "#FFFFFF",
-        main: {
-          100: "#C5D5EE",
-          200: "#ACBEDE",
-          300: "#90AFE5",
-          400: "#547ECD",
-          500: "#4666A2",
-        },
+        black: "#000000",
+        'green': '#11F1A6',
+        'green-darker': '#3BBD92',
+        'purple': '#A410FF',
+        'purple-darker': '#7523A7',
+        'primary-purple-darkest': '#6F00B2',
+        'magenta': '#EA4FFF',
+        'blue': '#5C67FF',
+        'blue-lighter': '#489CE8',
+        'blue-lightest': '#76FCFF',
+        'primary-blue-darkest': '#180A44',
+        // main: {
+        //   100: "#C5D5EE",
+        //   200: "#ACBEDE",
+        //   300: "#90AFE5",
+        //   400: "#547ECD",
+        //   500: "#4666A2",
+        // },
+      },
+      fontFamily: {
+        serif: ['Roboto', 'serif'],
+        sans: ['Roboto', 'sans-serif'],
       },
       borderWidth: {
         DEFAULT: "1px",
