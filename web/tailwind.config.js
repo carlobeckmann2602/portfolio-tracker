accf328545033f3ee6947b08205547c8ca5c15a6
Paul Kretschel <paul.kretschel@study.hs-duesseldorf.de>
2022-12-11 22:30:57 +0100

    Clean up Tailwind configuration

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
@@ -6,29 +8,25 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        white: "#FFFFFF",
-        black: "#000000",
-        'green': '#11F1A6',
-        'green-darker': '#3BBD92',
-        'purple': '#A410FF',
-        'purple-darker': '#7523A7',
-        'primary-purple-darkest': '#6F00B2',
-        'magenta': '#EA4FFF',
-        'blue': '#5C67FF',
-        'blue-lighter': '#489CE8',
-        'blue-lightest': '#76FCFF',
-        'primary-blue-darkest': '#180A44',
-        // main: {
-        //   100: "#C5D5EE",
-        //   200: "#ACBEDE",
-        //   300: "#90AFE5",
-        //   400: "#547ECD",
-        //   500: "#4666A2",
-        // },
+        highlight1: {
+          // Green
+          DEFAULT: "#11F1A6",
+          offset: "#3BBD92",
+        },
+        highlight2: {
+          // Purple
+          DEFAULT: "#A410FF",
+          offset: "#7523A7",
+        },
+        back: {
+          DEFAULT: "#180A44",
+          offset: "#6F00B2",
+        },
+        front: "white",
       },
       fontFamily: {
-        serif: ['Roboto', 'serif'],
-        sans: ['Roboto', 'sans-serif'],
+        sans: ["Roboto", ...defaultTheme.fontFamily.sans],
+        serif: ["Roboto Serif", ...defaultTheme.fontFamily.serif],
       },
       borderWidth: {
         DEFAULT: "1px",
