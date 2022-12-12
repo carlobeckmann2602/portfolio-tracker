571fb5d434ab77174a7af21076b7cd48dcc1b6bd
Paul Kretschel <paul.kretschel@study.hs-duesseldorf.de>
2022-12-12 15:53:42 +0100

    Fix PWA metadata for iOS

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -21,11 +21,11 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#6F00B2" />
+        <meta name="theme-color" content="#180A44" />
 
         <link rel="icon" type="image/png" sizes="144x144" href="icon-144.png" />
         <link rel="icon" type="image/png" sizes="512x512" href="icon-512.png" />
-        <link rel="apple-touch-icon" href="/icon-512.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 
         <meta
           name="viewport"
