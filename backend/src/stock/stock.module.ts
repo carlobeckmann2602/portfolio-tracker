aa2ef8f8777709d6fc7c27863acfd36b6f1dc176
philipp-vogel <philipp242@live.de>
2022-12-19 21:30:32 +0100

    changing imports

--- backend/src/stock/stock.module.ts
+++ backend/src/stock/stock.module.ts
@@ -3,10 +3,14 @@ import { StockService } from './stock.service';
 import { StockController } from './stock.controller';
 import { StockAPITasks } from 'src/stockAPITasks/stockAPITasks.module';
 import { StockAPITasksService } from 'src/stockAPITasks/stockAPITasks.service';
+import { UserService } from 'src/user/user.service';
+import { AuthService } from 'src/auth/auth.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
-  controllers: [StockController],
-  providers: [StockService, StockAPITasksService],
   imports: [StockAPITasks],
+  controllers: [StockController],
+  providers: [StockService, StockAPITasksService, UserService, AuthService, JwtService],
+  exports: [StockService],
 })
 export class StockModule {}
