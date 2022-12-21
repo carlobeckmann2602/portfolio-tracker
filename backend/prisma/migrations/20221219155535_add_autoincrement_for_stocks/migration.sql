d95000f225e913288dcbfdcd9063bc2ea6673822
Konstantin Haupt <konstantin.haupt@yahoo.de>
2022-12-21 20:43:23 +0100

    Delete old prisma migrations and add new cleanup-migration

--- backend/prisma/migrations/20221219155535_add_autoincrement_for_stocks/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-CREATE SEQUENCE "stock_id_seq";
-ALTER TABLE "Stock" ALTER COLUMN "id" SET DEFAULT nextval('stock_id_seq');
-ALTER SEQUENCE "stock_id_seq" OWNED BY "Stock"."id";
--- backend/prisma/migrations/20221219223014_add_table_for_jwt_blacklisting/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Tokens" (
-    "id" SERIAL NOT NULL,
-    "token" TEXT NOT NULL,
-
-    CONSTRAINT "Tokens_pkey" PRIMARY KEY ("id")
-);
