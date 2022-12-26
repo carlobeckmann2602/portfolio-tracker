fc39fac40a4f985eb587a46e6973e7c0026a8b10
Paul Kretschel <paul.kretschel@study.hs-duesseldorf.de>
2022-12-26 20:33:19 +0100

    Fix comments of stocks API lib

--- web/lib/backend/stocks/rest.ts
+++ web/lib/backend/stocks/rest.ts
@@ -36,7 +36,7 @@ type StockHoldingDTO = {
   totalAmount: number;
 };
 
-export type PortfolioDataDTO = {
+type PortfolioDataDTO = {
   portfoliovalue: number;
   stocksOnUser: StockHoldingDTO[];
 };
@@ -51,7 +51,6 @@ function createStockFromDTO({ histories, ...core }: StockDTO): Stock {
   };
 }
 
-/** Converts the REST API's data format of a stock holding to the local format. */
 function createStockHoldingFromDTO({
   totalAmount,
   totalValue,
@@ -73,19 +72,19 @@ function createPortfolioFromDTO(dto: PortfolioDataDTO): PortfolioData {
   };
 }
 
-/** Fetch the user's stock holdings from REST API. */
+/** Fetches the user's stock holdings. */
 export async function fetchPortfolioData() {
   const res = await authFetch(`${BACKEND_REST_URL}/users/me/stocks`);
   return createPortfolioFromDTO(await res.json());
 }
 
-/** Fetch the user's stock holdings from REST API. */
+/** Fetches public data of a specific stock. */
 export async function fetchStock(stockId: number) {
   const res = await fetch(`${BACKEND_REST_URL}/stocks/${stockId}`);
   return createStockFromDTO(await res.json());
 }
 
-/** Search stocks by name in REST API. */
+/** Searches stocks by name. */
 export async function fetchStockSearch(searchTerm: string) {
   const res = await fetch(`${BACKEND_REST_URL}/stocks?name=${searchTerm}`);
   const dtos: StockDTO[] = await res.json();
@@ -94,6 +93,7 @@ export async function fetchStockSearch(searchTerm: string) {
     .map((dto) => createStockFromDTO(dto));
 }
 
+/** Mutates the amount of a specific stock holding. */
 export async function fetchStockHoldingAmountMut({
   stockId,
   amountOffset,
