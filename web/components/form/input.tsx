42a3a676e5328d03b22863e5ac625d330a83d899
Paul Kretschel <paul.kretschel@study.hs-duesseldorf.de>
2022-12-28 17:14:55 +0100

    Brighten input symbol when field is focused/filled

--- web/components/form/input.tsx
+++ web/components/form/input.tsx
@@ -1,34 +1,50 @@
-import React from "react";
+import { ReactNode, RefObject, useState } from "react";
+import cn from "classnames";
 
 export type InputProps = {
   type?: string;
   name: string;
   placeholder?: string;
-  innerRef?: React.RefObject<HTMLInputElement>;
-  icon?: React.ReactNode;
+  innerRef?: RefObject<HTMLInputElement>;
+  icon?: ReactNode;
 };
 
 export const Input = ({
   type = "text",
   name,
-  innerRef,
   placeholder,
   icon,
-}: InputProps) => (
-  <div className="relative text-base form-group">
-    <label className="flex flex-col">
-      {icon && (
-        <div className="h-5 w-5 absolute box-border top-1/3 ml-5 pointer-events-none opacity-60">
-          {icon}
-        </div>
-      )}
-      <input
-        type={type}
-        name={name}
-        placeholder={placeholder}
-        className="border border-transparent outline-none border-solid focus:border-highlight1 p-3 pl-14 rounded-[10px] bg-front/10 font-light text-lg"
-        ref={innerRef}
-      />
-    </label>
-  </div>
-);
+  innerRef,
+}: InputProps) => {
+  const [isFocused, setIsFocused] = useState(false);
+  const [hasContent, setHasContent] = useState(false);
+
+  return (
+    <div className="relative text-base form-group">
+      <label className="flex flex-col">
+        {icon && (
+          <div
+            className={cn(
+              "h-5 w-5 absolute box-border top-1/3 ml-5 pointer-events-none transition",
+              !isFocused && !hasContent && "opacity-60"
+            )}
+          >
+            {icon}
+          </div>
+        )}
+        <input
+          type={type}
+          name={name}
+          placeholder={placeholder}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+          onInput={(evt) =>
+            setHasContent(!!(evt.target as HTMLInputElement).value)
+          }
+          className="border border-transparent outline-none border-solid focus:border-highlight1 p-3 pl-14 rounded-[10px] bg-front/10 font-light text-lg"
+          ref={innerRef}
+        />
+      </label>
+    </div>
+  );
+};
