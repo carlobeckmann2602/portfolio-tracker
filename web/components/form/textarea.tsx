563a2860262aa44b5a7d559856174418501b6fb9
Paul Kretschel <paul.kretschel@study.hs-duesseldorf.de>
2022-12-29 16:47:50 +0100

    Fix form components

--- web/components/form/textarea.tsx
+++ web/components/form/textarea.tsx
@@ -1,25 +1,18 @@
 import React from "react";
-import { InputLabel } from "./input_label";
 
 export type TextAreaProps = {
   name: string;
-  label?: string;
   placeholder?: string;
 };
 
-export const TextArea = ({ name, label, placeholder }: TextAreaProps) => {
-  const labelElement = label ? <InputLabel>{label}</InputLabel> : null;
-
-  return (
-    <div className="relative text-base form-group my-2">
-      <label className="flex flex-col">
-        {labelElement}
-        <textarea
-          name={name}
-          placeholder={placeholder}
-          className="border outline-none border-solid border-highlight1/50 focus:border-highlight1 p-1 rounded-sm"
-        />
-      </label>
-    </div>
-  );
-};
+export const TextArea = ({ name, placeholder }: TextAreaProps) => (
+  <div className="relative text-base form-group my-2">
+    <label className="flex flex-col">
+      <textarea
+        name={name}
+        placeholder={placeholder}
+        className="border outline-none border-solid border-highlight1/50 focus:border-highlight1 p-1 rounded-sm"
+      />
+    </label>
+  </div>
+);
