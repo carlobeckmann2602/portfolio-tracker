7e28c429adfa09068fa472866482e9a08d7a9a11
Ben Kräling <ben.kraeling@study.hs-duesseldorf.de>
2023-01-03 13:12:01 +0100

    Reset search when stocks were added

--- web/components/search/index.tsx
+++ web/components/search/index.tsx
@@ -58,89 +58,104 @@ export const Search = ({
     [foundStocks, portfolio]
   );
 
-  if (selectedStock) {
-    return (
-      <AddStockForm stock={selectedStock} onAdd={closeSearch}></AddStockForm>
-    );
-  }
-  return (
-    <div className="relative">
-      <div
-        className={cn(
-          "absolute -top-3 right-0 pointer-events-none transition duration-500 rounded-full overflow-hidden opacity-25",
-          isFetching ? "scale-100" : "scale-0"
-        )}
-      >
-        <BounceLoader color="white" size="3rem" />
-      </div>
-      <h2 className="text-2xl xs:text-3xl font-bold font-serif mt-8 mb-6">
-        Add stocks
-      </h2>
-      <div className="flex mt-4 mb-6 rounded-md bg-front/10">
-        <input
-          ref={inputRef}
-          type="text"
-          name="searchTerm"
-          placeholder="Search for a stock"
-          className="w-full outline-none bg-transparent py-4 pl-4"
-        />
-        <button
-          className="w-14 flex-shrink-0 text-2xl flex justify-center items-center"
-          onClick={() => {
-            const input = inputRef.current!;
-            if (searchTerm) {
-              setSearchTerm("");
-              input.value = "";
-            }
-            input.focus();
-          }}
-        >
-          <div className="pointer-events-none">
-            {searchTerm.length ? (
-              <FiPlus className="rotate-45" />
-            ) : (
-              <FiSearch />
-            )}
-          </div>
-        </button>
-      </div>
-      <div className="relative" style={{ minHeight: "8rem" }}>
-        {!searchTerm || searchIsFetching || results.length ? (
-          results.map(({ stock, inPortfolio }, i) => {
-            const disabled = portfolioIsFetching || inPortfolio;
+  const resetSearch = () => {
+    const input = inputRef.current!;
+    setSearchTerm("");
+    input.value = "";
+    closeSearch();
+  };
 
-            return (
-              <button
-                key={i}
-                className={cn(
-                  "flex justify-between w-full mb-4 transition",
-                  inPortfolio && !searchTerm
-                    ? "hidden"
-                    : disabled
-                    ? "opacity-25"
-                    : "rounded-md hover:bg-white/10"
-                )}
-                onClick={
-                  !inPortfolio ? () => setSelectedStock(stock) : undefined
-                }
-                disabled={disabled}
-              >
-                <SearchItem trend={2.5} name={stock.name} price={stock.price} />
-              </button>
-            );
-          })
-        ) : (
-          <p
-            className="font-light text-center mx-auto"
-            style={{ maxWidth: "10rem" }}
+  const stockForm = selectedStock ? (
+    <AddStockForm stock={selectedStock} onAdd={resetSearch}></AddStockForm>
+  ) : null;
+
+  return (
+    <div>
+      <div className={`relative ${selectedStock ? "block" : "hidden"}`}>
+        {stockForm}
+      </div>
+      <div className={`relative ${selectedStock ? "hidden" : "block"}`}>
+        <div
+          className={cn(
+            "absolute -top-3 right-0 pointer-events-none transition duration-500 rounded-full overflow-hidden opacity-25",
+            isFetching ? "scale-100" : "scale-0"
+          )}
+        >
+          <BounceLoader color="white" size="3rem" />
+        </div>
+        <h2 className="text-2xl xs:text-3xl font-bold font-serif mt-8 mb-6">
+          Add stocks
+        </h2>
+        <div className="flex mt-4 mb-6 rounded-md bg-front/10">
+          <input
+            ref={inputRef}
+            type="text"
+            name="searchTerm"
+            placeholder="Search for a stock"
+            className="w-full outline-none bg-transparent py-4 pl-4"
+          />
+          <button
+            className="w-14 flex-shrink-0 text-2xl flex justify-center items-center"
+            onClick={() => {
+              const input = inputRef.current!;
+              if (searchTerm) {
+                setSearchTerm("");
+                input.value = "";
+              }
+              input.focus();
+            }}
           >
-            No stocks found with name &quot;
-            {searchTerm.length < 12
-              ? searchTerm
-              : `${searchTerm.substring(0, 9)}...`}
-            &quot;.
-          </p>
-        )}
+            <div className="pointer-events-none">
+              {searchTerm.length ? (
+                <FiPlus className="rotate-45" />
+              ) : (
+                <FiSearch />
+              )}
+            </div>
+          </button>
+        </div>
+        <div className="relative" style={{ minHeight: "8rem" }}>
+          {!searchTerm || searchIsFetching || results.length ? (
+            results.map(({ stock, inPortfolio }, i) => {
+              const disabled = portfolioIsFetching || inPortfolio;
+
+              return (
+                <button
+                  key={i}
+                  className={cn(
+                    "flex justify-between w-full mb-4 transition",
+                    inPortfolio && !searchTerm
+                      ? "hidden"
+                      : disabled
+                      ? "opacity-25"
+                      : "rounded-md hover:bg-white/10"
+                  )}
+                  onClick={
+                    !inPortfolio ? () => setSelectedStock(stock) : undefined
+                  }
+                  disabled={disabled}
+                >
+                  <SearchItem
+                    trend={2.5}
+                    name={stock.name}
+                    price={stock.price}
+                  />
+                </button>
+              );
+            })
+          ) : (
+            <p
+              className="font-light text-center mx-auto"
+              style={{ maxWidth: "10rem" }}
+            >
+              No stocks found with name &quot;
+              {searchTerm.length < 12
+                ? searchTerm
+                : `${searchTerm.substring(0, 9)}...`}
+              &quot;.
+            </p>
+          )}
+        </div>
       </div>
     </div>
   );
