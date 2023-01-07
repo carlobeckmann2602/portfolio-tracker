cfca64b54e57899d2b071d57dd000ed9cca705e3
Marco Mojica <marco@mojica.de>
2023-01-07 15:24:50 +0100

    Add NetworkFirst caching strategy for /stocks

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,26 @@
 /** @type {import('next').NextConfig} */
 
+const runtimeCaching = require('next-pwa/cache')
+
 const withPWA = require('next-pwa')({
-  dest: 'public'
+  dest: 'public',
+  runtimeCaching: [
+    {
+      urlPattern: ({url}) => {
+        return url.pathname === '/stocks'
+      },
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'stocks',
+        expiration: {
+          maxEntries: 1024,
+          maxAgeSeconds: 24 * 60 * 60 // 24 hours
+        },
+        networkTimeoutSeconds: 2/10
+      }
+    },
+    ...runtimeCaching
+  ]
 })
 
 const config = {
