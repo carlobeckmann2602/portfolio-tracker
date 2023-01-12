5da80a17fa658f5e1886c8f58011bfaa4de18144
Konstantin Haupt <konstantin.haupt@yahoo.de>
2023-01-12 20:38:06 +0100

    Remove (now deprecated) mockdata logic

--- backend/src/stock/stock.controller.ts
+++ backend/src/stock/stock.controller.ts
@@ -1,164 +1,18 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { AuthService } from 'src/auth/auth.service';
-import { StockAPITasksService } from 'src/stockAPITasks/stockAPITasks.service';
-import { SplitService } from './split.service';
 import { StockService } from './stock.service';
 @ApiTags('stocks')
 @Controller('stocks')
 export class StockController {
-  mockMode = false;
-  mockStocks = [
-    {
-      id: 0,
-      symbol: 'ADS',
-      name: 'Adidas',
-      open: '137.5000',
-      close: '175.4600',
-      high: '184.2000',
-      low: '132.1000',
-      trend: '-0.42',
-      description:
-        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-      time: '2022-10-11T14:37:10.000Z',
-    },
-    {
-      id: 1,
-      symbol: 'CON',
-      name: 'Continental',
-      open: '54.5000',
-      close: '83.9000',
-      high: '156.2000',
-      low: '54.5000',
-      trend: '+1.98',
-      description:
-        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-      time: '2022-01-01T00:00:00.000Z',
-    },
-    {
-      id: 2,
-      symbol: 'EXA',
-      name: 'Example',
-      open: '135.6000',
-      close: '147.9000',
-      high: '173.2000',
-      low: '120.5000',
-      trend: '+3.65',
-      description:
-        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-      time: '2022-01-01T00:00:00.000Z',
-    },
-    {
-      id: 3,
-      symbol: 'DPD',
-      name: 'DPD',
-      open: '235.5000',
-      close: '211.9000',
-      high: '240.2000',
-      low: '180.5000',
-      trend: '-4.76',
-      description:
-        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-      time: '2022-01-01T00:00:00.000Z',
-    },
-    {
-      id: 4,
-      symbol: 'AAPL',
-      name: 'Apple',
-      open: '566.5000',
-      close: '600.9000',
-      high: '612.2000',
-      low: '566.5000',
-      trend: '+0.89',
-      description:
-        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-      time: '2022-01-01T00:00:00.000Z',
-    },
-    {
-      id: 5,
-      symbol: 'TEL',
-      name: 'Telekom',
-      open: '28.5600',
-      close: '34.9000',
-      high: '55.2000',
-      low: '25.5000',
-      trend: '-5.20',
-      description:
-        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-      time: '2022-01-01T00:00:00.000Z',
-    },
-    {
-      id: 6,
-      symbol: 'BASF',
-      name: 'BASF',
-      open: '175.5600',
-      close: '176.9000',
-      high: '178.2000',
-      low: '170.5000',
-      trend: '-0.89',
-      description:
-        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-      time: '2022-01-01T00:00:00.000Z',
-    },
-    {
-      id: 7,
-      symbol: 'LH',
-      name: 'Lufthansa',
-      open: '270.5600',
-      close: '279.9000',
-      high: '285.2000',
-      low: '234.5000',
-      trend: '+0.00',
-      description:
-        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-      time: '2022-01-01T00:00:00.000Z',
-    },
-    {
-      id: 8,
-      symbol: 'IBM',
-      name: 'IBM',
-      open: '125.0000',
-      close: '122.1000',
-      high: '127.2000',
-      low: '122.3000',
-      trend: '-12.00',
-      description:
-        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-      time: '2022-10-11T14:37:10.000Z',
-    },
-  ];
 
   constructor(
     private readonly stockService: StockService,
-    private readonly taskService: StockAPITasksService,
-    private readonly splitService: SplitService,
-    private readonly authService: AuthService,
   ) {}
-
-  // ----------------------------------------- //
-  // ONLY FOR TESTING PURPOSES, DELETE LATER   //
-  // ----------------------------------------- //
-  // @Get('/test/:name')
-  // async testAPI(@Param('name') stockName: string) {
-  //   // const testResponse = await this.taskService.requestStockAPI(stockName, ApiFunctions.AV_DAILY);
-  //   // const testResponse = this.taskService.insertUpdatedStock(
-  //   //   0,
-  //   //   await this.taskService.requestStockAPI('AAPL', ApiFunctions.AV_DAILY),
-  //   // );
-  //   this.taskService.insertHistoricDataCron();
-  //   return '';
-  // }
-  // ----------------------------------------- //
-  // ONLY FOR TESTING PURPOSES, DELETE LATER   //
-  // ----------------------------------------- //
 
   @Get()
   @ApiOperation({ summary: 'Returns all stocks that contain the given (sub)string in their name or symbol' })
   @ApiResponse({ status: 200, description: 'Returns all found stocks as an array of json-objects' })
   async findAll(@Query('name') stockName: string) {
-    if (this.mockMode) {
-      return this.mockStocks;
-    }
     return this.stockService.searchStocks(stockName); // !!!! Sec issue !!! we have to make sure that the query string is not malicious code -> in string casten
   }
 
@@ -167,13 +21,6 @@ export class StockController {
   @ApiResponse({ status: 200, description: 'Returns a json-object containing all information about the stock with the sid' })
   @ApiResponse({ status: 400, description: 'There was no stock with the given sid. No stock-object is returned' })
   findOne(@Param('id') id: number) {
-    if (this.mockMode) {
-      if (id >= 0 && id < 8) {
-        return this.mockStocks[id];
-      } else {
-        return [];
-      }
-    }
     return this.stockService.getStockWithHistory(+id);
   }
 }
--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -26,153 +26,6 @@ import { UserService } from './user.service';
 @ApiTags('users')
 @Controller('users')
 export class UserController {
-  mockUser = {
-    id: 1,
-    createdAt: '2022-11-10T13:25:43.741Z',
-    updatedAt: '2022-11-10T13:30:38.939Z',
-    email: 'mustermann@web.de',
-  };
-
-  mockUserEdited = {
-    id: 1,
-    createdAt: '2022-11-10T13:25:43.741Z',
-    updatedAt: '2022-11-11T11:11:38.939Z',
-    email: 'musterfrau@web.de',
-  };
-
-  mockStocksOnUser = {
-    stocks: [
-      {
-        stock: {
-          id: 0,
-          symbol: 'ADS',
-          name: 'Adidas',
-          open: '137.5000',
-          close: '175.4600',
-          high: '184.2000',
-          low: '132.1000',
-          trend: '-0.42',
-          description:
-            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-          time: '2022-10-11T14:37:10.000Z',
-        },
-        amount: 84,
-      },
-      {
-        stock: {
-          id: 1,
-          symbol: 'CON',
-          name: 'Continental',
-          open: '54.5000',
-          close: '83.9000',
-          high: '156.2000',
-          low: '54.5000',
-          trend: '+1.98',
-          description:
-            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-          time: '2022-01-01T00:00:00.000Z',
-        },
-        amount: 113,
-      },
-      {
-        stock: {
-          id: 2,
-          symbol: 'EXA',
-          name: 'Example',
-          open: '135.6000',
-          close: '147.9000',
-          high: '173.2000',
-          low: '120.5000',
-          trend: '+3.65',
-          description:
-            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-          time: '2022-01-01T00:00:00.000Z',
-        },
-        amount: 49,
-      },
-      {
-        stock: {
-          id: 3,
-          symbol: 'DPD',
-          name: 'DPD',
-          open: '235.5000',
-          close: '211.9000',
-          high: '240.2000',
-          low: '180.5000',
-          trend: '-4.76',
-          description:
-            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-          time: '2022-01-01T00:00:00.000Z',
-        },
-        amount: 13,
-      },
-      {
-        stock: {
-          id: 4,
-          symbol: 'AAPL',
-          name: 'Apple',
-          open: '566.5000',
-          close: '600.9000',
-          high: '612.2000',
-          low: '566.5000',
-          trend: '+0.89',
-          description:
-            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-          time: '2022-01-01T00:00:00.000Z',
-        },
-        amount: 2,
-      },
-      {
-        stock: {
-          id: 5,
-          symbol: 'TEL',
-          name: 'Telekom',
-          open: '28.5600',
-          close: '34.9000',
-          high: '55.2000',
-          low: '25.5000',
-          trend: '-5.20',
-          description:
-            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-          time: '2022-01-01T00:00:00.000Z',
-        },
-        amount: 4,
-      },
-      {
-        stock: {
-          id: 6,
-          symbol: 'BASF',
-          name: 'BASF',
-          open: '175.5600',
-          close: '176.9000',
-          high: '178.2000',
-          low: '170.5000',
-          trend: '-0.89',
-          description:
-            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-          time: '2022-01-01T00:00:00.000Z',
-        },
-        amount: 6,
-      },
-      {
-        stock: {
-          id: 7,
-          symbol: 'LH',
-          name: 'Lufthansa',
-          open: '270.5600',
-          close: '279.9000',
-          high: '285.2000',
-          low: '234.5000',
-          trend: '+0.00',
-          description:
-            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-          time: '2022-01-01T00:00:00.000Z',
-        },
-        amount: 87,
-      },
-    ],
-  };
-  mockMode = false;
   constructor(
     private readonly userService: UserService,
     private readonly authService: AuthService,
@@ -228,9 +81,6 @@ export class UserController {
   @ApiResponse({ status: 400, description: "There was a fatal error fetching the users' portfolio" })
   @ApiBearerAuth('JWT-auth')
   getStocksFromUser(@Req() req: Request) {
-    if (this.mockMode) {
-      return this.mockStocksOnUser;
-    }
     return this.portfolioService.getPortfolioData(req.user['userId']);
   }
 
@@ -242,9 +92,6 @@ export class UserController {
   @ApiResponse({ status: 200, description: 'Return success message' })
   @ApiBearerAuth('JWT-auth')
   removeStockFromUser(@Param('sid') sid: number, @Body() stockOnUserDto: StockOnUserDto, @Req() req: Request) {
-    if (this.mockMode) {
-      return `This action updates a user with id #${req.user['userId']} with the transmitted stock data`;
-    }
     return this.transactionService.addTransaction(
       req.user['userId'],
       +sid,
@@ -262,10 +109,6 @@ export class UserController {
   @ApiResponse({ status: 201, description: 'Return success message' })
   @ApiBearerAuth('JWT-auth')
   async addStockToUser(@Param('sid') sid: number, @Body() stockOnUserDto: StockOnUserDto, @Req() req: Request) {
-    if (this.mockMode) {
-      return `This action updates a user with id #${req.user['userId']} with the transmitted stock data`;
-    }
-
     try {
       return await this.transactionService.addTransaction(
         req.user['userId'],
