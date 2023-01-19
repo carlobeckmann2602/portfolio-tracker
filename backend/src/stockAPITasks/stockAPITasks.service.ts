8944dc3310c4e5ea6641d2b249716751f18cd54e
Konstantin Haupt <konstantin.haupt@yahoo.de>
2023-01-19 23:02:39 +0100

    Resolve ESLint Warnings

--- backend/src/stockAPITasks/stockAPITasks.service.ts
+++ backend/src/stockAPITasks/stockAPITasks.service.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/ban-types */
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import { PrismaService } from 'src/prisma/prisma.service';
